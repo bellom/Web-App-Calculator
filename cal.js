6e332205bea0995b6d3ddd77fdac4781775d3399
bellom <bellomsean@gmail.com>
2018-08-18 16:26:44 +0100

    commit

--- cal.js
+++ cal.js
@@ -1,36 +1,36 @@
-function multButton() {
-//get input 1 and 2 and multipy them
-    let val1 = $("#input1").val();
-    let val2 = $("#input2").val();
-    let mult = "0";
-    mult = val1*val2;
-    $('#result').html(mult);
-};
-
-function sumButton() {
-//get input 1 and 2 and sum them
-
-    let val1 = parseFloat($("#input1").val());
+function operations(elem){
+    let val1 = parseFloat($("#input1").val());        
     let val2 = parseFloat($("#input2").val());
-    let sum = "0";
-    sum = (val1+val2);
-    $('#result').html(sum);
-};
+    let result = "0";
+    console.log("testing");
+//switch btn using id
+    switch(elem.id) {
+        case 'mult-btn':
+            result = val1*val2;
+            break;
+        case 'sum-btn':
+            result = val1+val2;
+            break;
+        case 'divide-btn':
+            result = val1/val2;
+            break;
+        case 'sub-btn':
+            result = val1-val2;
+            break;
+    }                
+    $('#result').html(result)
+}
 
-function divideButton() {
-//get input 1 and 2 and divide them
-    let val1 = $("#input1").val();
-    let val2 = $("#input2").val();
-    let division = "0";
-    division = val1/val2;
-    $('#result').html(division);
-};
 
-function subButton() {
-//get input 1 and 2 and minus them
-    let val1 = $("#input1").val();
-    let val2 = $("#input2").val();
-    let sub = "0";
-    sub = val1-val2;
-    $('#result').html(sub);
-};
+
+
+
+//    if (elem.id == "mult-btn") {
+//        result = val1*val2;
+//    } else if (elem.id == "sum-btn") {
+//        result = val1+val2;
+//    } else if (elem.id == "divide-btn") {
+//        result = val1/val2;
+//    } else{
+//        result = val1-val2;
+//    }
